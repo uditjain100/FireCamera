ff180c417b3fa39e9d35f0965886990ad7001c21
Udit Jain <jain30udit@gmail.com>
2020-06-20 20:17:01 +0530

    Selection Work Added

--- app/src/main/java/udit/programmer/co/firecamera/MainActivity.kt
+++ app/src/main/java/udit/programmer/co/firecamera/MainActivity.kt
@@ -1,18 +1,8 @@
 package udit.programmer.co.firecamera
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.ProgressDialog
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,6 +21,10 @@ class MainActivity : AppCompatActivity() {
 
         camerax_btn.setOnClickListener {
             startActivity(Intent(this, CameraXActivity::class.java))
+        }
+
+        text_btn.setOnClickListener {
+            startActivity(Intent(this, TextRecognizerActivity::class.java))
         }
 
     }
---
+++ app/src/main/java/udit/programmer/co/firecamera/TextRecognizerActivity.kt
@@ -0,0 +1,74 @@
+package udit.programmer.co.firecamera
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+import kotlinx.android.synthetic.main.activity_text_recognizer.*
+import java.io.File
+import java.net.URI
+
+class TextRecognizerActivity : AppCompatActivity() {
+
+    lateinit var uri: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_recognizer)
+
+        pick_btn.setOnClickListener {
+            selectionWork()
+        }
+    }
+
+    private fun selectionWork() {
+        startActivityForResult(Intent(Intent.ACTION_PICK), 1234)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1234 && resultCode == Activity.RESULT_OK) {
+            displayWork(data)
+        }
+    }
+
+    private fun displayWork(data: Intent?) {
+        display_btn.setOnClickListener {
+            uri = data!!.data!!
+            image_display.setImageURI(uri)
+            textRecognizingWork()
+        }
+    }
+
+
+    private fun textRecognizingWork() {
+        text_recognizer_btn.setOnClickListener {
+            recognizing_work()
+        }
+    }
+
+    private fun recognizing_work() {
+        val image = FirebaseVisionImage.fromFilePath(this, uri)
+        val recognizer = FirebaseVision.getInstance().cloudTextRecognizer
+        recognizer.processImage(image).addOnSuccessListener {
+            process_Image(it)
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed :(", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun process_Image(it: FirebaseVisionText?) {
+        val blocks = it!!.textBlocks
+        if (blocks.size == 0) {
+            Toast.makeText(this, "No Text :(", Toast.LENGTH_LONG).show()
+            return
+        }
+        for (block in blocks)
+            image_name_tv.text = block.text
+    }
+}
