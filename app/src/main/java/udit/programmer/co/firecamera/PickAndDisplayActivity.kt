814e4a07c2d451833f96b34e05f06d95094c609f
Udit Jain <jain30udit@gmail.com>
2020-07-04 21:07:02 +0530

    Cropping with Firebase Uploading

--- app/src/main/java/udit/programmer/co/firecamera/PickAndDisplayActivity.kt
+++ app/src/main/java/udit/programmer/co/firecamera/PickAndDisplayActivity.kt
@@ -5,8 +5,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
 import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.activity_firebase.*
 import kotlinx.android.synthetic.main.activity_pick_and_display.*
 import kotlinx.android.synthetic.main.activity_pick_and_display.image_display
 import kotlinx.android.synthetic.main.activity_text_recognizer.*
@@ -14,32 +18,39 @@ import kotlinx.android.synthetic.main.activity_text_recognizer.*
 class PickAndDisplayActivity : AppCompatActivity() {
 
     private var imageUri: Uri? = null
-
+    lateinit var firebaseStorage: StorageReference
+    private lateinit var filePath: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_and_display)
-
-//        pad_pick_btn.setOnClickListener {
-//            startActivityForResult(Intent(Intent.ACTION_PICK), 12345)
-//        }
+        firebaseStorage = FirebaseStorage.getInstance().reference
         pad_pick_btn.setOnClickListener {
             CropImage.activity().setAspectRatio(1, 1).start(this)
         }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == 12345) {
-            pad_display_btn.setOnClickListener {
-                image_display.setImageURI(data!!.data!!)
-            }
-        }
         if (resultCode == Activity.RESULT_OK && requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            var result = CropImage.getActivityResult(data)
-            imageUri = result.uri
+            imageUri = CropImage.getActivityResult(data).uri
             Picasso.get().load(imageUri).into(image_display)
+
+            filePath = firebaseStorage.child("Photos")
+            filePath.putFile(CropImage.getActivityResult(data).uri).addOnCompleteListener {
+                Toast.makeText(this, "Uploaded Successfully :)", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Uploading Failed $it :(", Toast.LENGTH_LONG).show()
+            }.continueWithTask {
+                filePath.downloadUrl
+            }.addOnCompleteListener {
+                if (it.isComplete) {
+                    Toast.makeText(this, "Uri Task Completed :)", Toast.LENGTH_LONG).show()
+                    imageUri = it.result
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, "Uri Failed $it :(", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
